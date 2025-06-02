fc1207b1754f04716c8cec25d198d35cfbb55ffb
Angel Añez <angdavid2910@gmail.com>
2025-06-01 23:02:58 -0400

    style: improve certificate modal responsive

--- src/components/Modals/ModalCertificate.jsx
+++ src/components/Modals/ModalCertificate.jsx
@@ -20,11 +20,11 @@ const ModalCertificate = ({ isOpen, onClose, certificate }) => {
     <>
       <div className="fixed inset-0 z-40 bg-black/50"></div>
       <div
-        className="fixed inset-0 flex items-center justify-center z-50 p-4"
+        className="fixed inset-0 flex items-center justify-center z-50 md:p-4"
         onClick={handleOverlayClick}
       >
         <article className="bg-white shadow-xl max-w-[540px] pb-8 px-8 w-full overflow-y-auto relative">
-          <section className="absolute flex items-center justify-end right-10">
+          <section className="md:absolute flex items-center justify-end right-10">
             <div className="bg-black pt-16 pb-4 px-4 flex items-end">
               <QRCode
                 value={certificate.link}
@@ -33,7 +33,7 @@ const ModalCertificate = ({ isOpen, onClose, certificate }) => {
               />
             </div>
           </section>
-          <section className="flex flex-col gap-y-4 mt-24">
+          <section className="flex flex-col gap-y-4 mt-4 md:mt-24">
             <h2 className="font-semibold text-2xl mb-1">{certificate.name}</h2>
             <p className="font-light">
               <span className="font-medium">Tipo de producto: </span>
